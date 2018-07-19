9a3eaf453cc05f2577b3d4ef0de5734d16b9ca49
JasonWatts <jawatts@westmont.edu>
2018-07-19 13:53:08 -0700

    formatting and cleanup

---
+++ wordLists.py
@@ -0,0 +1,15 @@
+wordList1 = ["enchanting", "amazing", "colourful", "delightful", "honeydew", "delicate","shimmering", "fantastic","luminous", "ladybug", "butterfly", "happiness","blueberry","hydroflask", "burrito"]
+wordList2 = ["visions", "distance", "conscience", "process", "chaos","holy", "chubby","humor","grapefruit","idle","item","label","bunny","woman","birthday","frisky","nuisance","fire","water", "eyebrow","nostril","peanut"
+,"lady","legal","locate","major","minus","moment","motor","music","nation","notion","obey","odor","oval",
+"paper","photo","pilot","pony","pretend","private","pupil","radar","razor","reason","recess","regard","resist","rival","robot","rotate","rumor","secret","silent","siren","soda"]
+wordList3 = ["superstitious", "contradicting", "overwhelming","undermining", "intermittent", "malificent", "eternity"]
+wordList4 = ["true", "dark", "cold", "warm", "great", "a", "all", "and", "are", "as", "be", "by", "day", 
+"did", "each", "few", "for", "get", "have", "he", "him", "his", "i", "in", "is", "it","long","man"
+,"me","more","much","my","new","not","now","of","off","old","one","on","or","out","same","she","so","state","then","that","they","then","time","to","up","war","was"]
+wordList5 = ["visions", "distance", "conscience", "process", "chaos","holy","humor","idle","item","label"
+,"lady","legal","locate","major","minus","moment","motor","music","nation","notion","obey","odor","oval",
+"paper","photo","pilot","pony","pretend","private","pupil","radar","razor","reason","recess","regard","resist","rival","robot","rotate","rumor","secret","silent","siren","soda"]
+wordList6 = ["beautiful", "imperfect", "subtley"]
+wordList7 = ["visions", "distance", "conscience", "process", "chaos","holy","humor","idle","item","label"
+,"lady","legal","locate","major","minus","moment","motor","music","nation","notion","obey","odor","oval",
+"paper","photo","pilot","pony","pretend","private","pupil","radar","razor","reason","recess","regard","resist","rival","robot","rotate","rumor","secret","silent","siren","soda"]
